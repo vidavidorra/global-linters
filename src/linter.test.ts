22235b1b8f6c8a98532066be30d3cafacca1df85
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-08-13 21:16:17 +0200

    test: add tests for Linter
    Possible tests were added. Some tests would require mocking `shell.exec` which turned out to be quit a challange actually, so I am seeking help for that. In the meantime those tests are skipped here.

---
+++ src/linter.test.ts
@@ -0,0 +1,84 @@
+import { Linter } from './linter';
+import commandExists from 'command-exists';
+
+describe('Linter', (): void => {
+  const defaultLinterName = 'hadolint';
+  const defaultLinterRange = '>1.0.0';
+
+  describe('With non-exixting linter.', (): void => {
+    let mockCommandExists;
+    beforeEach((): void => {
+      mockCommandExists = jest
+        .spyOn(commandExists, 'sync')
+        .mockImplementation((): boolean => {
+          return true;
+        });
+    });
+
+    afterEach((): void => {
+      mockCommandExists.mockRestore();
+    });
+
+    test('Trows if the passed linter name does not exist.', (): void => {
+      expect((): void => {
+        const linter = new Linter(defaultLinterName, defaultLinterRange);
+        expect(linter.Name() === defaultLinterName);
+      }).toThrow();
+    });
+  });
+
+  describe('With exixting linter.', (): void => {
+    let mockCommandExists;
+    beforeEach((): void => {
+      mockCommandExists = jest
+        .spyOn(commandExists, 'sync')
+        .mockImplementation((): boolean => {
+          return true;
+        });
+
+      const ms =
+        'ShellCheck - shell script analysis tool\n' +
+        'version: 1.1.1\n' +
+        'license: GNU General Public License, version 3\n' +
+        'website: https://www.shellcheck.net';
+
+      console.log(ms);
+    });
+
+    afterEach((): void => {
+      mockCommandExists.mockRestore();
+    });
+
+    test.skip('Accepts a valid linter name.', (): void => {
+      /**
+       * Mock `shell.exec` to return
+       * 'ShellCheck - shell script analysis tool\n' +
+       * 'version: 1.1.1\n' +
+       * 'license: GNU General Public License, version 3\n' +
+       * 'website: https://www.shellcheck.net';
+       */
+      expect((): void => {
+        new Linter(defaultLinterName, defaultLinterRange);
+      }).not.toThrow();
+    });
+
+    test.skip('Accepts a valid range.', (): void => {
+      /**
+       * Mock `shell.exec` to return
+       * 'ShellCheck - shell script analysis tool\n' +
+       * 'version: 1.1.1\n' +
+       * 'license: GNU General Public License, version 3\n' +
+       * 'website: https://www.shellcheck.net';
+       */
+      expect((): void => {
+        new Linter(defaultLinterName, defaultLinterRange);
+      }).not.toThrow();
+    });
+
+    test('Throws if range is not satified.', (): void => {
+      expect((): void => {
+        new Linter(defaultLinterName, '>=99.0.0');
+      }).toThrow();
+    });
+  });
+});
