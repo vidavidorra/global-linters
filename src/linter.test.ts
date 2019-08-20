1a8eab5e141070fe0dd9b927d8cedff9217ca6f3
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-08-20 21:04:17 +0200

    test: use mocked ShellExec for Linter tests
    The ShellExec wrapper/helper was added in a previous commit and can now be used for these tests!

--- src/linter.test.ts
+++ src/linter.test.ts
@@ -1,5 +1,8 @@
+import * as ShellExec from './helpers/shell-exec';
 import { Linter } from './linter';
 import commandExists from 'command-exists';
+
+jest.mock('./helpers/shell-exec');
 
 describe('Linter', (): void => {
   const defaultLinterName = 'hadolint';
@@ -32,41 +35,40 @@ describe('Linter', (): void => {
     });
   });
 
-  describe.skip('With existing linter.', (): void => {
+  describe('With existing linter.', (): void => {
     let mockCommandExists;
+    let mockShellExec;
     beforeEach((): void => {
       mockCommandExists = jest
         .spyOn(commandExists, 'sync')
         .mockImplementation((): boolean => {
           return true;
         });
+
+      mockShellExec = jest.spyOn(ShellExec, 'ShellExec').mockImplementation(
+        (): ShellExec.ShellExecResult => {
+          const stdout =
+            'ShellCheck - shell script analysis tool\n' +
+            'version: 1.1.1\n' +
+            'license: GNU General Public License, version 3\n' +
+            'website: https://www.shellcheck.net';
+          return { code: 0, stdout, stderr: null };
+        }
+      );
     });
 
     afterEach((): void => {
       mockCommandExists.mockRestore();
+      mockShellExec.mockRestore();
     });
 
     test('Accepts a valid linter name.', (): void => {
-      /**
-       * Mock `shell.exec` to return
-       * 'ShellCheck - shell script analysis tool\n' +
-       * 'version: 1.1.1\n' +
-       * 'license: GNU General Public License, version 3\n' +
-       * 'website: https://www.shellcheck.net';
-       */
       expect((): void => {
         new Linter(defaultLinterName, defaultLinterRange);
       }).not.toThrow();
     });
 
     test('Accepts a valid range.', (): void => {
-      /**
-       * Mock `shell.exec` to return
-       * 'ShellCheck - shell script analysis tool\n' +
-       * 'version: 1.1.1\n' +
-       * 'license: GNU General Public License, version 3\n' +
-       * 'website: https://www.shellcheck.net';
-       */
       expect((): void => {
         new Linter(defaultLinterName, defaultLinterRange);
       }).not.toThrow();
