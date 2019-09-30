d25140d930b556cbb5d05bd09811295314b00c7a
Jeroen de Bruijn <vidavidorra@gmail.com>
2019-09-30 19:17:41 +0200

    refactor: move arguments to interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from './arguments';
 export * from './glob';
 export * from './global-linters';
 export * from './helpers';
+export * from './interface';
 export * from './linter';
 export * from './linters';
